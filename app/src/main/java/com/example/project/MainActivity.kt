f41de8bf6ca344e301b7e3c867849cc17ca978a8
Daniil Shestopalov <d.shestopalov@letmecode.org>
2025-02-19 17:04:12 +0300

    began practice: Compose quadrant

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.project
 
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,6 +28,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.project.ui.theme.ProjectTheme
 
+// https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems?continue=https://developer.android.com/courses/pathways/android-basics-compose-unit-1-pathway-3%23codelab-https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems#3
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,40 +38,59 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.primary
                 ) {
-                    Task()
+
                 }
             }
         }
     }
 
-
     @Composable
-    fun Task() {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+    fun Quadrant() {
+        Row(
+            horizontalArrangement = Arrangement.Absolute.SpaceBetween
         ) {
-            Image(
-                painter = painterResource(R.drawable.ic_task_completed),
-                contentDescription = null
+            TextBlock(
+                title = "Text composable",
+                message = "Displays text and follows the recommended Material Design guidelines.",
+                color = Color(0xFFEADDFF)
             )
-            Text(
-                text = "All tasks conpleted",
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
-            )
-            Text(
-                text = "Nice work",
-                fontSize = 16.sp
+            TextBlock(
+                title = "Image composable",
+                message = "Creates a composable that lays out and draws a given Painter class object.",
+                color = Color(0xFFD0BCFF)
             )
         }
     }
 
-    @Preview(showBackground = true)
     @Composable
-    fun TaskPreview() {
+    fun TextBlock(
+        title: String,
+        message: String,
+        color: Color
+    ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.background(color).padding(16.dp)
+
+            ) {
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = message
+                )
+
+        }
+    }
+
+
+@Preview(showBackground = true)
+@Composable
+    fun Preview() {
         ProjectTheme {
-            Task()
+            Quadrant()
         }
     }
 }
