ec296f76c367ebf20aa5b4bae260da7fa25d8cb7
Daniil Shestopalov <d.shestopalov@letmecode.org>
2025-02-19 16:40:42 +0300

    practice: Task Manager

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.example.project
 
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,13 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.project.ui.theme.ProjectTheme
@@ -40,7 +32,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.primary
                 ) {
-                    JetpackComposeTutorial()
+                    Task()
                 }
             }
         }
@@ -48,42 +40,32 @@ class MainActivity : ComponentActivity() {
 
 
     @Composable
-    fun JetpackComposeTutorial() {
-        Column {
+    fun Task() {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Image(
-                painter = painterResource(R.drawable.bg_compose_background),
+                painter = painterResource(R.drawable.ic_task_completed),
                 contentDescription = null
             )
-
             Text(
-                text = "Jetpack Compose tutorial",
-                fontSize = 24.sp,
-                modifier = Modifier.padding(16.dp)
-            )
-
-
-            Text(
-                text = stringResource(R.string.jetpack_compose_is),
-                textAlign = TextAlign.Justify,
-                modifier = Modifier.padding(horizontal = 16.dp)
-
+                text = "All tasks conpleted",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
             )
             Text(
-                text = stringResource(R.string.in_this_tutorial),
-                textAlign = TextAlign.Justify,
-                modifier = Modifier.padding(16.dp)
+                text = "Nice work",
+                fontSize = 16.sp
             )
         }
     }
 
     @Preview(showBackground = true)
     @Composable
-    fun JetpackComposeTutorialPreview() {
+    fun TaskPreview() {
         ProjectTheme {
-            JetpackComposeTutorial()
+            Task()
         }
     }
-
-
-
 }
